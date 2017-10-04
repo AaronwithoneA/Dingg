abc00afc0a0eb4e8707c5f9c223ebcf5d1624b64
AaronwithoneA <acutler84@gmail.com>
2017-10-04 12:10:22 -0700

    Set uppath to serve index.html file

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ import express from 'express';
 
 var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
