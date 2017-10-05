52f7fe85189a785835041ca0829c11557f3d93c9
AaronwithoneA <acutler84@gmail.com>
2017-10-05 12:10:22 -0700

    Tell app to listen on the port

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
