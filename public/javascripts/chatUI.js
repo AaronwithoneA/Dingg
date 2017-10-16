c8e8e963ac1bf989cb005bfee8ef6e32d9772150
AaronwithoneA <acutler84@gmail.com>
2017-10-16 12:10:22 -0700

    Instantiate DOM query selectors in chatUI class constructor

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -2,4 +2,9 @@ import Chat from './chat.js';
 
 function ChatUI(socket) {
   this.chat = new Chat(socket);
+  this.input = document.querySelector('input')
+  this.form = document.querySelector('form')
+  this.msgList = document.querySelector('ul#msg-list')
+  this.roomList = document.querySelector('ul#room-list')
+   this.room = document.querySelector('#room')
 }
